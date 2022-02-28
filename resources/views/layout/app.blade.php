682de83b5903dc6c7e47f5a86674b6c84709a5f1
Septian <haiseptian@gmail.com>
2022-02-28 13:13:46 +0700

    Fixing Title

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cuteeth Dental Therapist | System</title>
+    <title>E-Prescription</title>
 
     <link rel="icon" href="{{ asset('dist/favicon.ico') }}" type="image/gif" sizes="16x16">
     <!-- Google Font: Source Sans Pro -->
